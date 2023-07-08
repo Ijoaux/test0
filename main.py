711e8098670f949bc4977c98a43c6212711d95fb
Ijoaux <136209487+Ijoaux@users.noreply.github.com>
2023-07-08 09:00:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,55 @@
-meme_dict = {
-            "CRINGE": "Sesuatu yang sangat aneh atau memalukan",
-            "LOL": "Tanggapan umum terhadap sesuatu yang lucu",
-            "ROFL" : "tanggapan terhadap lelucon",
-            "SHEESH" : 'sedikit ketidaksetujuan',
-            "CREEPY" : 'menakutkan, tidak menyenangkan',
-            "AGGRO" : 'untuk menjadi agresif/marah',
-            }
+import discord
+from title1 import pass_gen
+from discord.ext import commands
 
-word = input('Ketik kata yang tidak Kamu mengerti (gunakan huruf kapital semua!):')
+# Variabel intents menyimpan hak istimewa bot
+intents = discord.Intents.default()
+# Mengaktifkan hak istimewa message-reading
+intents.message_content = True
+# Membuat bot di variabel klien dan mentransfernya hak istimewa
+#client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-if word in meme_dict.keys():
-    print(meme_dict[word])
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def password(ctx, f = 10):
+    await ctx.send(pass_gen(f))
+
+bot.run("MTEyNDUxMDg0MTU0NzUyNjMwNw.Gpa0z4.WLDFlRrR5VRDS6VbXaphjOduRnguRSrYKou6fg")
+
+
+# @client.event
+# async def on_ready():
+#     print(f'Kita telah masuk sebagai {client.user}')
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
+#     if message.content.startswith('$halo'):
+#         await message.channel.send("Hi!")
+#     elif message.content.startswith('$bye'):
+#         await message.channel.send("😀")
+#     elif message.content.startswith('$Trade'):
+#         await message.channel.send("Succesfully DENIED")
+#     if message.content.startswith('$random pass'):
+#         await message.channel.send(pass_gen(10))
+
+#     else:
+#         await message.channel.send(message.content)
+
     
-    
-else:
-    print("kata tidak ditemukan")
+
+#client.run("MTEyNDUxMDg0MTU0NzUyNjMwNw.Gpa0z4.WLDFlRrR5VRDS6VbXaphjOduRnguRSrYKou6fg")
+

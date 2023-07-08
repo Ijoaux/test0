3f350e323f14edb69da1a43f9b3dec08061f2e1a
Ijoaux <136209487+Ijoaux@users.noreply.github.com>
2023-07-08 08:57:33 +0700

    Update title1.py

--- title1.py
+++ title1.py
@@ -1,11 +1,18 @@
 import random
-x = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-print(random.choices(x,k = 10))
-t = ""
-a = int(input("masukan panjang pasword:"))
-for i in range(a):
-    y = random.choice(x)
-    t = t + y
+
+def pass_gen(a):
+    x = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    #print(random.choices(x,k = 10))
+    t = ""
+    #a = int(input("masukan panjang pasword:"))
+    for i in range(a):
+        y = random.choice(x)
+        t = t + y
+    return t
+
+
+
     
     
-print(t)
+
+
